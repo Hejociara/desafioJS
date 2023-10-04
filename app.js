f5e72a9baac0bd78158750d1d0e910a46e40d0a9
Hejociara <hejociarav@gmail.com>
2023-10-04 11:52:09 -0300

    update

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 alert ('Boas vindas ao nosso site!');
+let nome = prompt('Qual é o seu nome?');
+
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+
+    
+if (idade >= 18){
+alert ('Pode tirar habilitação')
+}
+else if (idade <= 18) {
+    alert('Não pode tirar habilitação');
+}
